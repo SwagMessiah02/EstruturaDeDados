3d0ef0641993a11b0db89df8a7456ed63f79070b
Vinicius Mendes <156609327+SwagMessiah02@users.noreply.github.com>
2024-10-26 10:25:44 -0300

    Update teste.c

--- C/teste.c
+++ C/teste.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include "arvorebinaria.h"
 
+// Compara dois inteiros
 int compareint(int *a, int *b) {
     if(*a > *b)
         return 1;
@@ -11,6 +12,7 @@ int compareint(int *a, int *b) {
         return 0;
 }
 
+// Compara dois caracteres
 int comparechar(char *a, char *b) {
     int v1 = (int)*a;
     int v2 = (int)*b;
@@ -24,18 +26,22 @@ int comparechar(char *a, char *b) {
 }
 
 int main(void) {
-    int v1[5] = {1, 5, 3, 0, 2};
-    char v2[5] = {'b', 'f', 'c', 'd', 'i'};
+    // Arrays para teste
+    int vetor1[5] = {1, 5, 3, 0, 2};
+    char vetor2[5] = {'b', 'f', 'c', 'd', 'i'};
 
+    // Ponteiros para as respectivas funções de comparação
     int (*ptr)(int *a, int *b) = &compareint;
     int (*ptr2)(char *a, char *b) = &comparechar;
 
+    // Inicializa a estrutura de dados
     Arvore *arvore = criarArvore();
 
     for(int i = 0;i < 5;i++) {
-        inserirNo(arvore, criarNo(&v2[i]), (int (*)(void**,void**))ptr2);
+        inserirNo(arvore, criarNo(&vetor2[i]), (int (*)(void**,void**))ptr2); // Insere os valores do vetor v2 na árvore
     }
 
+    // Imprime os elementos da árvore 'em-ordem'
     imprimirEmOrdem(arvore->raiz);
 
     return 0;
