36ebe8cce0b7ecbeafb771837fab48d0be89e11e
Vinicius Mendes <156609327+SwagMessiah02@users.noreply.github.com>
2024-10-31 21:53:21 -0300

    Update main.c

--- C/ArvoreBinaria/main.c
+++ C/ArvoreBinaria/main.c
@@ -3,15 +3,15 @@
 
 #define LENGTH(v) sizeof(v)/sizeof(v[0])
 
-int main(void)
-{
-   Arvore *arvore = criar_arvore();
-
-   const int vetor_1[5] = {1,0,5,3,2};
+const int vetor_1[5] = {1,0,5,3,2};
    const char vetor_2[5] = {'d','b','e','f','a'};
    const char* vetor_3[4] = {"Okarun", "Momo", "JiJi", "Vovó Turbo"};
 
-   const int length = LENGTH(vetor_1); 
+const int length = LENGTH(vetor_1);
+
+int main(void)
+{
+   Arvore *arvore = criar_arvore();
 
    for (int i = 0; i < length; i++) {
       insere(arvore, criar_folha(&vetor_1[i]), INT);
