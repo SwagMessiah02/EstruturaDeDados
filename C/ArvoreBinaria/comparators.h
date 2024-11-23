ae6177c7f581d43163d5f40eabc278534a48f2b2
Vinicius Mendes <156609327+SwagMessiah02@users.noreply.github.com>
2024-11-23 17:55:26 -0300

    Delete C/ArvoreBinaria/comparators.h

--- C/ArvoreBinaria/comparators.h
+++
@@ -1,8 +0,0 @@
-#ifndef COMPARATORS_H
-#define COMPARATORS_H
-
-int comp_int(const int *a, const int *b);
-int comp_char(const char *a, const char *b);
-int comp_string(const char *a, const char *b);
-
-#endif //COMPARATORS_H
